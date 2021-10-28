abe52c051782702fdf6fb6df8dc97e2c270814c9
hyokyun <gyrbs22@gmail.com>
2021-10-28 16:33:28 +0900

    folder logic and added some folder css

--- Quest04/skeleton/desktop.js
+++ Quest04/skeleton/desktop.js
@@ -28,9 +28,8 @@ class Icon {
 			this.isClicked = true
 			this.offsetX = e.offsetX;
 			this.offsetY = e.offsetY;
-			this.currentObject = e.target
-
-		}	
+			this.currentObject = e.target;
+		}
 	}
 
 	mousemove(e){
@@ -50,7 +49,54 @@ class Icon {
 
 //Folder 속성과 행동 정의
 class Folder {
-	
+	constructor(isClicked, offsetX, offsetY, currentObject){
+		this.isClicked = isClicked;
+		this.offsetX = offsetX;
+		this.offsetY = offsetY;
+		this.currentObject = currentObject;
+	}
+
+	mousedown(e){
+		if(e.target.classList.contains('button')){
+			
+			this.isClicked = true
+			this.offsetX = e.offsetX;
+			this.offsetY = e.offsetY;
+			this.currentObject = e.target;
+		}else if(e.target.classList.contains('folderContent')){
+			
+			this.isClicked = true
+			this.offsetX = e.offsetX;
+			this.offsetY = e.offsetY;
+			this.currentObject = e.target;
+		}
+	}
+
+	mousemove(e){
+		if(this.isClicked == true){
+			this.currentObject.style.position = 'absolute';
+			this.currentObject.style.left = e.pageX - this.offsetX + 'px';	
+			this.currentObject.style.top = e.pageY - this.offsetY + 'px';
+		}else{
+			return ;
+		}
+	}
+
+	mouseup(){
+		this.isClicked = false;
+	}
+
+	//activatie folder window
+	getFolderWindow(e, folderWindow){
+		if(e.target.classList.contains('button')){
+			folderWindow.classList.remove('hidden');
+		}
+	}
+
+	removeFolderWindow(folderWindow){
+		folderWindow.classList.add('hidden');
+	}
+
 };
 
 //보여지는 화면 관련한 속성과 행동 정의
