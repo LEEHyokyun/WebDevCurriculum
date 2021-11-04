378f36e9a3649ce8333191076be7fff911081600
hyokyun <gyrbs22@gmail.com>
2021-11-04 14:56:41 +0900

    fileupload logic - class applied

--- Quest05/skeleton/notepad.js
+++ Quest05/skeleton/notepad.js
@@ -3,10 +3,10 @@ class tabAction {
 		this.checkList = checkList;
 		this.clickAreas = clickAreas;
 		this.connectTabstoContent = connectTabstoContent;
-		this.connectContentstoTabs = connectContentstoTabsx;
+		this.connectContentstoTabs = connectContentstoTabs;
 	}
 
-	getTabClicked(checkList, clickAreas){
+	setTabClicked(checkList, clickAreas, connectContentstoTabs){
 
 		for(let element of clickAreas){
 			element.addEventListener('click', (e)=>clickEvent(e));
@@ -22,7 +22,8 @@ class tabAction {
 			//click itself
 			}else if(checkList.length > 0 && checkList.includes(e.target.id)){
 				checkList.pop(e.target.id);
-				e.target.classList.toggle('clickArea');
+				e.target.classList.remove('contentLine');
+				removeContentEvent(e);
 			//click others
 			}else if(checkList.length > 0 && !(checkList.includes(e.target.id))){
 				let previousObject = document.getElementById(checkList[0]);
@@ -32,10 +33,20 @@ class tabAction {
 				checkList.pop();
 				checkList.push(e.target.id);
 				}
+
+			function removeContentEvent(e){
+				let targetTab = document.getElementById(e.target.id);
+				let targetContentId = targetTab.dataset.tabTarget;
+				let targetContent = document.getElementById(targetContentId.split('#')[1]);		
+					
+				targetContent.classList.add('display');		
+			}
 		};
+
+		
 	}
 
-	getContentWithTabsClicked(connectTabstoContent, connectContentstoTabs){
+	setContentWithTabsClicked(connectTabstoContent, connectContentstoTabs){
 		//for content click event
 		for(let tab of connectTabstoContent){
 			tab.addEventListener('click', ()=>{
@@ -50,3 +61,38 @@ class tabAction {
 	}
 	
 };
+
+class fileUpload{
+	constructor(reader, preview, fileInput){
+		this.reader = reader;
+		this.preview = preview;
+		this.fileInput = fileInput;
+	}
+
+	setUploadFile(reader, preview, fileInput){
+		fileInput.addEventListener('change', whenfileReaderSelected);
+
+		function handleEvent(e){
+			console.log('img.src');
+			preview.src = reader.result;
+
+			localStorage.setItem('0', preview.src)
+		};
+		
+
+		function whenfileReaderSelected(e){
+			let selectedFile = fileInput.files[0];
+
+			if(selectedFile){
+				console.log(fileInput.files)
+				reader.addEventListener('load', handleEvent);
+			}	
+				
+			console.log('image.src is ', preview.src);
+			console.log('fileInput.files[0] is ', selectedFile);
+				
+			reader.readAsDataURL(selectedFile);
+			console.log('readAsDataURL');
+			};
+	}
+}
