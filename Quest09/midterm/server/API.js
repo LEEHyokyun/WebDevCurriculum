d4d9163fe476f39cfa7c5076c96a227799e59507
hyokyun <gyrbs22@gmail.com>
2021-11-12 14:11:42 +0900

    final code - API/client server with logics

--- Quest09/midterm/server/API.js
+++ Quest09/midterm/server/API.js
@@ -1,9 +1,16 @@
+//PORT 3000
 const express = require('express');
 const app = express();
-
 const path = require('path');
 
-const fs = require('fs');
+const getUrlParams = require('../router/getUrlParams.js')
+const getFileName = require('../router/getFileName.js');
+const fileLoad = require('../router/fileload.js');
+const fileRead = require('../router/fileread.js');
+const fileDelete = require('../router/filedelete.js')
+
+let getAction = '';
+let fileName = '';
 
 app.use(express.static(path.join(__dirname, )))
 
@@ -12,5 +19,26 @@ app.get('/', (req, res)=>{
     //res.sendFile(path.join(__dirname, '../', 'index.html'));
     //res.send(data);
 });
+
+app.get('/api/:action/:filename', (req, res) => {
+    res.send(getUrlParams(req, res));
+
+    getAction = getUrlParams(req, res);
+    fileName = getFileName(req, res);
+
+    switch(getAction){
+        case 'fileload': {
+            fileLoad(fileName);
+            //console.log(fileName);
+        }
+        case 'fileread' : {
+            fileRead(fileName);
+        }
+        case 'filedelete' : {
+            fileDelete(fileName);
+        }
+    };
+});
+
 
 module.exports = app;
