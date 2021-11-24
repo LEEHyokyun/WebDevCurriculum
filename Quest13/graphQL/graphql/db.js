027feda31f18c9a1b03d73bcd245717f31a929a6
hyokyun <gyrbs22@gmail.com>
2021-11-24 15:49:18 +0900

    graphql : data update

--- Quest13/graphQL/graphql/db.js
+++ Quest13/graphQL/graphql/db.js
@@ -1,18 +1,23 @@
+const { Model } = require('sequelize/dist');
 let userDB = require('../models/model.js');
 
 module.exports = {
     userDB,
     
     getAllUserInfor: () => {
+        
         let findedDB = userDB.findAll()
         
         console.log(findedDB);
+        
         return findedDB;
+
+        //return userDB;
     },
 
     //외부DB를 가져오는 것은 적절한 method와 비구조화를 통해 진행
     getUserInfor: async (userID) => {
-        let selectedDB =[]
+        let selectedDB =[];
         selectedDB = await userDB.findOne({
             where: {
                 userID: userID
@@ -22,6 +27,20 @@ module.exports = {
         console.log(selectedDB);
 
         return selectedDB;
+    },
+
+    //data update
+    setUserData: async (userID, userPW) => {
+        
+        await userDB.create({
+            userID: userID,
+            userPW: userPW
+        });
+
+        let newDB = userDB.findAll();
+
+        if(newDB) return newDB;
     }
+    
     
 }
--- Quest13/graphQL/graphql/resolvers.js
+++ Quest13/graphQL/graphql/resolvers.js
@@ -1,10 +1,11 @@
-const {getAllUserInfor, getUserInfor} = require('./db.js'); 
+const {getAllUserInfor, getUserInfor, setUserData} = require('./db.js'); 
 
 const resolvers = {
     Query: {
         //named by resolvers => named by declared
         getAllUserInfor: () => getAllUserInfor(),
-        getUserInfor: (_, {userID}) => getUserInfor(userID)
+        getUserInfor: (_, {userID}) => getUserInfor(userID),
+        setUserData: (_, {userID, userPW}) => setUserData(userID, userPW)
     }
 }
 
